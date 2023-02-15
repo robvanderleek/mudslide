2feb644839fb0e0c682dff5ddebd4fe9501debb7
Rob van der Leek <robvanderleek@gmail.com>
2023-02-15 08:34:40 +0100

    Fix build

--- tests/whatsapp.test.ts
+++ tests/whatsapp.test.ts
@@ -6,3 +6,10 @@ test('parse geo location', () => {
     expect(result[0]).toBe(5);
     expect(result[1]).toBe(10);
 })
+
+test('parse geo location, round coords', () => {
+    const  result = parseGeoLocation('5.123456789', '10.123456789');
+
+    expect(result[0]).toBe(5.12346);
+    expect(result[1]).toBe(10.12346);
+})
