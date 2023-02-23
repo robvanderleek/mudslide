650bf9c7c96fafa366a7f41e534fbfcf3510cd5d
Rob van der Leek <robvanderleek@gmail.com>
2023-02-23 22:50:34 +0100

    First version, refactored user/group code

---
+++ src/commands.ts
@@ -0,0 +1,116 @@
+import signale from "signale";
+import {
+    checkLoggedIn,
+    checkValidFile,
+    getWhatsAppId,
+    initWASocket,
+    parseGeoLocation, sendFileHelper,
+    sendImageHelper,
+    terminate
+} from "./whatsapp";
+
+export async function sendMessage(recipient: string, message: string) {
+    checkLoggedIn();
+    const socket = await initWASocket();
+    socket.ev.on('connection.update', async (update) => {
+        const {connection} = update
+        if (connection === 'open') {
+            const whatsappId = await getWhatsAppId(socket, recipient);
+            signale.await(`Sending message: "${message}" to: ${whatsappId}`);
+            message = message.replace(/\\n/g, '\n');
+            await socket.sendMessage(whatsappId, {text: message});
+            signale.success('Done');
+            terminate(socket, 3);
+        }
+    });
+}
+
+export async function sendImage(recipient: string, path: string, options: { caption: string | undefined }) {
+    checkValidFile(path);
+    checkLoggedIn();
+    const socket = await initWASocket();
+    socket.ev.on('connection.update', async (update) => {
+        const {connection} = update
+        if (connection === 'open') {
+            const whatsappId = await getWhatsAppId(socket, recipient);
+            signale.await(`Sending image file: "${path}" to: ${whatsappId}`);
+            await sendImageHelper(socket, whatsappId, path, options);
+        }
+    });
+}
+
+export async function sendFile(recipient: string, path: string) {
+    checkValidFile(path);
+    checkLoggedIn();
+    const socket = await initWASocket();
+    socket.ev.on('connection.update', async (update) => {
+        const {connection} = update
+        if (connection === 'open') {
+            const whatsappId = await getWhatsAppId(socket, recipient);
+            signale.await(`Sending file: "${path}" to: ${whatsappId}`);
+            await sendFileHelper(socket, whatsappId, path);
+        }
+    });
+}
+
+export async function sendLocation(recipient: string, latitude: string, longitude: string) {
+    checkLoggedIn();
+    const geolocation = parseGeoLocation(latitude, longitude);
+    const socket = await initWASocket();
+    socket.ev.on('connection.update', async (update) => {
+            const {connection} = update
+            if (connection === 'open') {
+                const whatsappId = await getWhatsAppId(socket, recipient);
+                signale.await(`Sending location: ${geolocation[0]}, ${geolocation[1]} to: ${whatsappId}`);
+                await socket.sendMessage(whatsappId, {
+                    location: {
+                        degreesLatitude: geolocation[0],
+                        degreesLongitude: geolocation[1]
+                    }
+                });
+                signale.success('Done');
+                terminate(socket, 3);
+            }
+        }
+    );
+}
+
+export async function me() {
+    checkLoggedIn();
+    const socket = await initWASocket();
+    socket.ev.on('connection.update', async (update) => {
+        const {connection} = update
+        if (connection === 'open') {
+            const user = await socket.user
+            signale.log(`Current user: ${user?.id}`);
+            terminate(socket);
+        }
+    });
+}
+
+export async function listGroups() {
+    checkLoggedIn();
+    const socket = await initWASocket();
+    socket.ev.on('connection.update', async (update) => {
+        const {connection} = update
+        if (connection === 'open') {
+            const groupData = await socket.groupFetchAllParticipating()
+            for (const group in groupData) {
+                signale.log(`{"id": "${groupData[group].id}", "subject": "${groupData[group].subject}"}`);
+            }
+            terminate(socket);
+        }
+    });
+}
+
+export async function sendGroupMessage(id: string, message: string) {
+    await sendMessage(`${id}@g.us`, message);
+}
+
+export async function sendGroupImage(id: string, path: string, options: { caption: string | undefined }) {
+    await sendImage(`${id}@g.us`, path, options);
+}
+
+export async function sendGroupLocation(id: string, latitude: string, longitude: string) {
+    await sendLocation(`${id}@g.us`, latitude, longitude);
+}
--- src/whatsapp.ts
+++ src/whatsapp.ts
@@ -130,8 +130,28 @@ export async function logout() {
     });
 }
 
-export async function sendImageHelper(socket: any, whatsappId: string, path: string, options: { caption: string | undefined }) {
-    const payload = {image: fs.readFileSync(path), caption: options.caption}
+export async function getWhatsAppId(socket: any, recipient: string) {
+    if (recipient.endsWith('@s.whatsapp.net') || recipient.endsWith('@g.us')) {
+        return recipient;
+    } else if (recipient === 'me') {
+        const user = await socket.user;
+        if (user) {
+            const phoneNumber = user.id.substring(0, user.id.indexOf(':'));
+            return `${phoneNumber}@s.whatsapp.net`;
+        }
+    }
+    return `${recipient}@s.whatsapp.net`;
+}
+
+export async function sendImageHelper(socket: any, whatsappId: string, filePath: string, options: { caption: string | undefined }) {
+    const payload = {image: fs.readFileSync(filePath), caption: options.caption}
+    await socket.sendMessage(whatsappId, payload);
+    signale.success('Done');
+    terminate(socket, 3);
+}
+
+export async function sendFileHelper(socket: any, whatsappId: string, filePath: string) {
+    const payload = {document: fs.readFileSync(filePath), fileName: path.basename(filePath)}
     await socket.sendMessage(whatsappId, payload);
     signale.success('Done');
     terminate(socket, 3);
