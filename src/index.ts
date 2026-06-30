03f5aaf3ea65061b773826a3a2accfcadeb03333
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2026-06-30 14:56:57 +0300

    feat: ✨ There should be a Timeout parameter in the commands. (#395)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import {program} from "commander";
+import {Command, Option, program} from "commander";
 import {globalOptions, loginWithPairingCode, loginWithQrCode, logout, mudslideFooter} from "./whatsapp";
 import {
     listGroupParticipants,
@@ -13,6 +13,7 @@ import {
     sendPoll
 } from "./commands";
 import {bootstrap} from 'global-agent';
+import signale from "signale";
 
 const packageJson = require('../package.json');
 
@@ -42,68 +43,94 @@ program.on('option:proxy', () => {
     // @ts-ignore
     global.GLOBAL_AGENT.HTTPS_PROXY = process.env.HTTPS_PROXY;
 });
-program
-    .command('login')
-    .description('Login to WhatsApp')
-    .option('--pairing-code', 'Use pairing code instead of QR code')
-    .action((options) => options.pairingCode ? loginWithPairingCode() : loginWithQrCode());
-program
-    .command('logout')
-    .description('Logout from WhatsApp')
-    .action(() => logout());
+program.addOption(new Option('--timeout <sec>', 'Command timeout').default(10).argParser(parseInt));
 
-function configureCommands() {
+program.hook('preAction', (command: Command) => {
+    const timeout = command.getOptionValue('timeout');
+    setTimeout(() => {
+        signale.error('Action timed out');
+        process.exit(1);
+    }, timeout * 1000);
+});
+
+function configureBasicCommands() {
+    program
+        .command('login')
+        .description('Login to WhatsApp')
+        .option('--pairing-code', 'Use pairing code instead of QR code')
+        .action((options) => options.pairingCode ? loginWithPairingCode() : loginWithQrCode());
+
+    program
+        .command('logout')
+        .description('Logout from WhatsApp')
+        .action(() => logout());
+
     program
         .command('me')
         .description('Show current user details')
         .action(() => me());
-    program
-        .command('groups')
-        .description('List all your groups')
-        .action(() => listGroups());
+}
+
+function configureSendCommands() {
     program
         .command('send <recipient> <message>')
         .description('Send message')
         .action((recipient, message, options) => sendMessage(recipient, message, options));
+
     program
         .command('send-image <recipient> <file>')
         .option('--caption <text>', 'Caption text')
         .description('Send image file')
         .action((recipient, file, options) => sendImage(recipient, file, options));
+
     program
         .command('send-file <recipient> <file>')
         .option('--caption <text>', 'Caption text')
         .option('--type <document|audio|video>', 'File type', 'document')
         .description('Send file')
         .action((recipient, file, options) => sendFile(recipient, file, options));
+
     program
         .command('send-location <recipient> <latitude> <longitude>')
         .allowUnknownOption()
         .description('Send location')
         .action((recipient, latitude, longitude) => sendLocation(recipient, latitude, longitude));
+
     program
         .command('send-poll <recipient> <name>')
         .option('--item <text>', 'Poll item (repeatable option)', (val, prev: Array<string>) => prev.concat([val]), [])
         .option('--selectable <count>', 'Number of selectable items', '1')
         .description('Send poll')
         .action((recipient, name, options) => sendPoll(recipient, name, options));
+}
+
+function configureGroupCommands() {
+    program
+        .command('groups')
+        .description('List all your groups')
+        .action(() => listGroups());
+
+    program
+        .command('list-group <group-id>')
+        .description('List group participants')
+        .action((groupId) => listGroupParticipants(groupId));
+
     program
         .command('add-to-group <group-id> <phone-number>')
         .allowUnknownOption()
         .description('Add group participant')
         .action((groupId, phoneNumber) => mutateGroup(groupId, phoneNumber, 'add'));
+
     program
         .command('remove-from-group <group-id> <phone-number>')
         .allowUnknownOption()
         .description('Remove group participant')
         .action((groupId, phoneNumber) => mutateGroup(groupId, phoneNumber, 'remove'));
-    program
-        .command('list-group <group-id>')
-        .description('List group participants')
-        .action((groupId) => listGroupParticipants(groupId));
 }
 
-configureCommands();
+configureBasicCommands();
+configureSendCommands();
+configureGroupCommands();
 program.addHelpText('after', `
 
 Examples:
